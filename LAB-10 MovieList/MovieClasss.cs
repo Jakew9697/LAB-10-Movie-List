5acbc3edfadce4119ea647bc30d4dc896d3095d6
Jake <jworsham9697@gmail.com>
2020-04-30 17:40:35 -0500

    first commit for lab 10

---
+++ LAB-10 MovieList/MovieClasss.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB_10_MovieList
+{
+    class MovieClasss
+    {
+        //fields
+
+        private string title;
+
+        private string genre;
+
+        //properties
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        
+        public string Genre
+        {
+            get
+            {
+                return genre;
+            }
+            set
+            {
+                genre = value;
+            }
+        }
+
+        //constructors
+        public MovieClasss(string _title, string _genre)
+        {
+            title = _title;
+
+            genre = _genre;
+        }
+
+        public MovieClasss()
+        {
+            title = "default Movie";
+
+            genre = "default Genre";
+
+        }
+
+        //methods
+        public void PrintMovie()
+        {
+            Console.WriteLine(genre + " " + title + " ");
+        }
+
+        
+    }
+}
---
+++ LAB-10 MovieList/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LAB_10_MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MovieClasss> movies = new List<MovieClasss>();
+
+            movies.Add(new MovieClasss("Die Hard", "Action"));
+
+            movies.Add(new MovieClasss("Kill Bill v 2", "Mystery"));
+
+            movies.Add(new MovieClasss("Inception", "Thriller"));
+
+            movies.Add(new MovieClasss("Finding Nemo", "Adventure"));
+
+            movies.Add(new MovieClasss("Matrix", "Action"));
+
+
+            bool cont = true;
+            bool validInput = false;
+            string category = "";
+            while (cont)
+            {
+                validInput = false;
+
+                while (!validInput)
+                {
+
+
+                    Console.WriteLine("There are 5 movies in the list what genre are you interested in? \n1 Action \n2 Mystery \n3 Thriller \n4 Adventure ");
+                    string input = Console.ReadLine().Trim();
+                    try
+                    {
+                        int movieIndex = int.Parse(input);
+                        if (movieIndex == 1)
+                        {
+                            category = "Action";
+                        }
+                        else if (movieIndex == 2)
+                        {
+                            category = "Mystery";
+                        }
+                        else if (movieIndex == 3)
+                        {
+                            category = "Thriller";
+                        }
+                        else if (movieIndex == 4)
+                        {
+                            category = "Adventure";
+                        }
+                        else
+                        {
+                            throw new Exception("Invalid number entry 1-4 only");
+                        }
+
+                        validInput = true;
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e.Message);
+                        
+                    }
+                }
+                for (int i = 0; i < movies.Count; i++)
+                {
+                    if (category == movies[i].Genre)
+                    {
+                        Console.WriteLine(movies[i].Title);
+                    }
+                }
+
+
+                validInput = false;
+                while (!validInput)
+                {
+                    Console.WriteLine("Would you like to look at the movies in the other genres? y/n? ");
+                    string userContinue = Console.ReadLine().ToLower().Trim();
+                    if (userContinue == "y")
+                    {
+                        validInput = true;
+                        cont = true;
+                    }
+                    else if (userContinue == "n")
+                    {
+                        validInput = true;
+                        cont = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is an invalid input please enter (y) or (n) ");
+                    }
+                }
+            }
+        }
+
+    }
+}
+
